8f9028c296346cc64286aa5faed67523f0bcd460
littlepants22 <cmontgomery1997@gmail.com>
2023-12-06 20:19:27 -0600

    add ui for game

--- RPS.js
+++ RPS.js
@@ -1,3 +1,6 @@
+let playerWinCounter = 0;
+let comWinCounter = 0;
+let resultsWindow = document.querySelector('#results-window');
 function getComputerChoice(){
     let num = Math.floor(Math.random() * 3);
     let choice = "";
@@ -14,103 +17,105 @@ function getComputerChoice(){
     return choice;
 }
 
-// generate random number between 1 and 3 
-// map numbers to choices
-// return computer choice
-
-function getUserChoice(){
-    let userChoice = prompt("choose rock, paper, or scissors");
-
-    return userChoice;
-}
-
-function compareResults(comChoice, userInput){
+function playRound(comChoice, userInput){
+    let playerScoreDisplay = document.querySelector('#player-score');
+    let comScoreDisplay = document.querySelector('#computer-score');
+    let result = '';
     switch(comChoice){
         case("rock"): 
             if(userInput === "rock"){
-                return "tied";
+                result = "tied";
             }
             // if user is rock, result is tie
             if(userInput === "paper"){
-                return "win";
+                playerWinCounter++;
+                result = "win";
             }
             // if user is paper, result is win
             if(userInput === "scissors"){
-                return "loss";
+                comWinCounter++;
+                result = "loss";
             }
             // if user is scissors, result is loss 
         break;
         case("paper"): 
             if(userInput === "rock"){
-                return "loss";
+                comWinCounter++;
+                result = "loss";
             }
             if(userInput === "paper"){
-                return "tied";
+                result = "tied";
             }
             if(userInput === "scissors"){
-                return "win";
+                playerWinCounter++;
+                result = "win";
             }
 
         break;
         case("scissors"): 
             if(userInput === "rock"){
-                return "win";
+                playerWinCounter++;
+                result = "win";
             }
             if(userInput === "paper"){
-                return "loss";
+                comWinCounter++;
+                result = "loss";
             }
             if(userInput === "scissors"){
-                return "tied";
+                result = "tied";
             }
         break;
     }
+    resultsWindow.textContent = result;
+    playerScoreDisplay.textContent = playerWinCounter;
+    comScoreDisplay.textContent = comWinCounter;
+    checkWinner();
 }
 
+
+function gameSetup(){
+    let buttons = document.querySelectorAll('#button-container > button');
+    buttons.forEach(button => {
+        button.addEventListener('click', (e) => {
+            console.log(e.target.id);
+            playRound(getComputerChoice(), e.target.id)
+            
+        })
+    })
+}
+gameSetup()
+
+//start game
+//click choice
+//declare round winner
+//add point to round winner
+//check for winner 
 
 // get user Input 
 // compare results
 // declare winner
 
-function game(){
-
-    let comWinCount = 0;
-
-    let userWinCount = 0;
-
-    for( i = 0; i < 5; i++){
-        switch(compareResults(getComputerChoice(), getUserChoice())){
-            case("win"):
-                userWinCount++;
-                console.log("You win the round, user score is " + userWinCount);
-            break;
-            case("tied"):
-                console.log("Round tied " + " user score is " + userWinCount + " com score is " + comWinCount);
-            break;
-            case("loss"):
-                comWinCount++;
-                console.log("Computer wins the round, com score is " + comWinCount); 
-            break; 
-
-        }
-    } 
-    if(userWinCount > comWinCount){
-        console.log("You win! :D");
+function checkWinner(){
+    let rock = document.querySelector("#rock")
+    let paper = document.querySelector("#paper")
+    let scissors = document.querySelector("#scissors")
+    if(comWinCounter == 5){
+        resultsWindow.textContent = 'looooooooser'
+        rock.disabled = true;
+        paper.disabled = true;
+        scissors.disabled = true;
     }
-    if(userWinCount < comWinCount){
-        console.log("You lose! >:(");
-    }
-    if(userWinCount == comWinCount){
-        console.log("Tied :/");
-    }
+    if(playerWinCounter == 5){
+        resultsWindow.textContent = 'its the best day ever :D' 
+        rock.disabled = true;
+        paper.disabled = true;
+        scissors.disabled = true;
+    }   
+    
 }
+
+
 
 // if user score is higher they win
 // if com score is higher they win
 // if scores are the same its a tie
-
-game();
-
-// play a round
-// keep score
-// do that 5 times
-// declare winner
